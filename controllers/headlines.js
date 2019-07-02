97e67e79a68d6b469a0e2fb95055f13bce9e0c0f
Margarita <m_mg489@outlook.com>
2019-07-02 00:53:27 -0400

    updated headlines.js controller

---
+++ controllers/headlines.js
@@ -0,0 +1,40 @@
+// Bring in our scrape script and makeDate scripts
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+//Bring in the Headline and Note Mongoose models
+var Headline = require("../models/Headline");
+
+module.exports = {
+    //fetch will run the scrape function and grab all articles and insert into the headline collection in DB
+    fetch: function(cb) {
+        scrape(function(data) {
+            var articles = data;
+            for (var i=0; i< articles.length; i ++) {
+                articles[i].date = makeDate();
+                articles[i].saved = false;
+            }
+
+            Headline.collection.insertMany(articles, {ordered:false}, function(err, docs) {
+                cb(err, docs);
+            });
+        });
+    },
+    delete: function(query, cb){
+        Headline.remove(query, cb);
+    },
+    get: function(query, cb) {
+        Headline.find(query)
+        .sort({
+            _id: -1
+        })
+        .exec(function(err, doc){
+            cb(doc);
+        });
+    },
+    update: function(query, cb) {
+        Headline.update({_id: query._id}, {
+            $set: query
+        },{}, cb);
+    }
+}
