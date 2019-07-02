114582aadbb057f8997298581d451d2585bcd1ac
Margarita <m_mg489@outlook.com>
2019-07-02 01:40:21 -0400

    CRUD routes for articles

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,13 @@
+//Server Routes
+//=============
+
+//Bring in the Scrape funtion from our scripts directory
+var scrape = require("../scripts/scrape");
+
+//Bring headlines and notes from the controller
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
 module.exports= function(router) {
     //this route renders the homepage
     router.get("/", function(req,res) {
@@ -6,5 +16,71 @@ module.exports= function(router) {
     // This route renders the saved handlebars page
     router.get("/saved", function(req,res) {
         res.render("saved");
+    });
+
+    router.get("/api/fetch", function(req, res) {
+        headlinesController.fetch(function(err, docs) {
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "No new articles today. Check back tomorrow!"
+                });
+            }
+            else {
+                res.json({
+                    message: "Added " + docs.insertedCount + " new articles!"
+                });
+            }
+        });
+    });
+    // grab all the headlines in the database
+    router.get("/api/headlines", function(req, res) {
+        // this will return everything unless the user specifies something 
+        var query = {};
+        if (req.query.saved) {
+            query = req.query;
+        }
+        headlinesController.get(query, function(data){
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/headlines/:id", function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function(err, data){
+            res.json(data);
+        });
+    });
+
+    //update function
+    router.patch("/api/headlines", function(req, res) {
+        headlinesController.update(req.body, function(err, data){
+            res.json(data);
+        });
+    });
+    //router to grab all the notes associated with an article
+    router.get("/api/notes/:headline_id?", function(req,res){
+        var query = {};
+        if (req.params.headline_id) {
+            query._id = req.params.headline_id;
+        }
+
+        notesController.get(query, function(err, data) {
+            res.json(data);
+        });
+    });
+    //route to delete
+    router.delete("/api/notes/:id", function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        notesController.delete(query, function(err, data){
+            res.json(data);
+        });
+    });
+    //route to post notes
+    router.post("/api/notes", function(req,res){
+        notesController.save(req.body, function(data){
+            res.json(data);
+        });
     });
 }
